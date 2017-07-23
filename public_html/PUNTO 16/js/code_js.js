272f54b6544cca2fbb209c680e01606d26e89ac3
WOLFGANG <WOLFGANG@10.72.132.25>
2017-07-23 00:05:23 -0500

    Easy. terminé el ejercicio No 16.

---
+++ public_html/PUNTO 16/js/code_js.js
@@ -0,0 +1,6 @@
+var side1 = prompt("Please enter the value of the lade A");
+var side2 = prompt("Please enter the value of the lade A");
+var perimeter = (side1 * side1) + (side2 * side2);
+console.log("The perimeter of the rectangle is equal to .... " + perimeter);
+perimeter = side1 * side2;
+console.log("The area of the rectangle is equal to .... " + perimeter);
