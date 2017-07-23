84afd70bfde89a362f69af90ca5f5bb29a05d7b1
WOLFGANG <WOLFGANG@10.72.132.25>
2017-07-22 23:54:27 -0500

    Easy. termino el ejercicio No 15.

---
+++ public_html/PUNTO 15/js/code_js.js
@@ -0,0 +1,7 @@
+var age = Number(prompt("How old are you"));
+if (age >= 18) {
+  console.log("You are of age, Welcome, You can Login to the dance");
+} else
+{
+  console.log("You are a minor of age, Sorry Baby, you can not login");
+}
