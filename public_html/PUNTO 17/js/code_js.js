ccb7a810355d4f89c6912ff118d1ec07bb7cfb83
WOLFGANG <WOLFGANG@10.72.132.25>
2017-07-23 00:12:49 -0500

    Easy. terminé el ejercicio No 17.

---
+++ public_html/PUNTO 17/js/code_js.js
@@ -0,0 +1,6 @@
+var a = prompt("Enter the value of A ....");
+var b = prompt("Enter the value of B ....");
+var c = prompt("Enter the value of C ....");
+console.log("Y = B X C ÷ A ");
+var result = ((b * c) / a);
+console.log("THE RESULT OF THE RULE OF 3 ES EQUAL TO .... " + result);
